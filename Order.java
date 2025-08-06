8ba2d57823c603dd3d8e02e3ee1fb0b0c4908da5
RyanTran07 <rtran934@gmail.com>
2025-08-05 17:41:04 -0700

    Made changes to Order.java

--- Order.java
+++ Order.java
@@ -1,13 +1,20 @@
 /*
- * Name: Ryan Tran, Krish Kowkuntla, Erin Law
+ * Name: Ryan Tran, Krish Kowkuntla, Erin Law, Carson Luu, Grace Truong
+ *
  * Description: This program is a menu-driven interaction system where a user
  *              continuosly enters their burger choice and how many burgers
  *              they want until they indicate that they want to end their order.
  *              It then asks them for what type of customer they are and prints
  *              out their order quantity, the subtotal for each burger option,
  *              their subtotal for all burgers, the tax charged, and the total
- *              bill.
+ *              bill. It implements an abstract Order class, which contains
+ *              all of the instance variables, appropriate getters and setters
+ *              and abstract methods that  implemented in the
+ *              Order.java file.
  *
+ * Note: To compile and run, this program was placed in a folder/directory. The
+ *       Driver.java, Order.java, OrderBurger.java and OrderInterface.java
+ *       are all in the same folder/directory
  *
  */
 
